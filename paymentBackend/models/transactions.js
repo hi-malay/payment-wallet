7e9cf3dd08d35abf58b91cf87da4d8e0acf90e95
malay3 <malay@goodhealthapp.com>
2021-04-09 10:41:55 +0530

    added

---
+++ paymentBackend/models/transactions.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const transSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    date: { type: Number, required: true },
+    amount: { type: Date, required: true },
+
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('transSchema', transSchema);
